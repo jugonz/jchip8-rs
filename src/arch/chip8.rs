44459c43d125b3fee943a143027306f1f575d09f
Julian Gonzalez <jugonzalez12@gmail.com>
2024-09-25 17:28:41 -0700

    Split out Opcode and chip8 into new files
    Experimental because rust is confusing.
    Will arch/mod.rs's contents be necessary at the end?
    Who even knows.

--- src/arch/chip8.rs
+++ src/arch/chip8.rs
@@ -1,49 +1,6 @@
+#![allow(dead_code)]
 use crate::gfx;
-
-struct Opcode {
-    value: u16,
-    // Registers are usize because rust forces indexing to be as usize,
-    // even when the indexes are of a smaller type and thus are in (compile-time) bounds.
-    // TODO: We will enforce that they are in bounds at execution time.
-    xreg: usize,  // "X" register in opcode table. 0 if unused, max 16.
-    yreg: usize,  // 0 if unused.
-    literal: u16, // Last three hex digits of our opcode.
-}
-
-impl Opcode {
-    pub fn new(value: u16) -> Opcode {
-        let xreg = ((value >> 8) & 0xF) as usize;
-        let yreg = ((value >> 4) & 0xF) as usize;
-
-        Opcode {
-            value,
-            xreg,
-            yreg,
-            literal: value & 0xFFF,
-        }
-    }
-}
-
-impl std::fmt::Display for Opcode {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "Value: 0x{:x} Xreg: {} Yreg: {} Literal: {}",
-            self.value, self.xreg, self.yreg, self.literal
-        )
-    }
-}
-
-impl Default for Opcode {
-    fn default() -> Opcode {
-        Opcode {
-            value: 0,
-            xreg: 0,
-            yreg: 0,
-            literal: 0,
-        }
-    }
-}
+use crate::arch::opcode::Opcode;
 
 struct Chip8 {
     // Core structural components.
---
+++ src/arch/opcode.rs
@@ -0,0 +1,45 @@
+#[non_exhaustive] // don't allow init of Opcode outside this file
+pub struct Opcode {
+    pub value: u16,
+    // Registers are usize because rust forces indexing to be as usize,
+    // even when the indexes are of a smaller type and thus are in (compile-time) bounds.
+    // TODO: We will enforce that they are in bounds at execution time.
+    pub xreg: usize,  // "X" register in opcode table. 0 if unused, max 16.
+    pub yreg: usize,  // 0 if unused.
+    pub literal: u16, // Last three hex digits of our opcode.
+}
+
+impl Opcode {
+    pub fn new(value: u16) -> Opcode {
+        let xreg = ((value >> 8) & 0xF) as usize;
+        let yreg = ((value >> 4) & 0xF) as usize;
+
+        Opcode {
+            value,
+            xreg,
+            yreg,
+            literal: value & 0xFFF,
+        }
+    }
+}
+
+impl std::fmt::Display for Opcode {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Value: 0x{:x} Xreg: {} Yreg: {} Literal: {}",
+            self.value, self.xreg, self.yreg, self.literal
+        )
+    }
+}
+
+impl Default for Opcode {
+    fn default() -> Opcode {
+        Opcode {
+            value: 0,
+            xreg: 0,
+            yreg: 0,
+            literal: 0,
+        }
+    }
+}
