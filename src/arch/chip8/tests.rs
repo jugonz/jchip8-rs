f14688d1378460bf7fbcb85c89c89850b0feec1d
Julian Gonzalez <jugonzalez12@gmail.com>
2024-09-26 16:23:27 -0700

    Tighten up test syntax

--- src/arch/chip8/tests.rs
+++ src/arch/chip8/tests.rs
@@ -7,11 +7,10 @@ fn run_opcode(c8: &mut Chip8, instruction: u16) {
 
 #[test]
 fn test_skip_instruction() {
-    let mut c8 = Chip8::new(false);
+    let mut c8 = Chip8::new(true);
 
     // First, add the literal (A3) to a register.
-    c8.opcode = Opcode::new(0x71A3);
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x71A3);
     c8.increment_pc();
 
     // Now, check the PC.
@@ -19,36 +18,28 @@ fn test_skip_instruction() {
 
     // Now, check that an instruction is skipped when
     // comparing the literal.
-    c8.opcode = Opcode::new(0x31A3);
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x31A3);
     c8.increment_pc();
-
     assert_eq!(c8.pc, 0x206);
 
     // Now, check that an instruction is NOT skipped
     // when comparing the same literal.
-    c8.opcode = Opcode::new(0x41A3);
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x41A3);
     c8.increment_pc();
-
     assert_eq!(c8.pc, 0x208);
 
     // Now, check that comparing two identical registers
     // leads to an instruction skip.
-    c8.opcode = Opcode::new(0x72A3); // Add literal to another register.
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x72A3); // Add literal to another register.
     c8.increment_pc();
-
-    c8.opcode = Opcode::new(0x5120);
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x5120);
     c8.increment_pc();
-
     assert_eq!(c8.pc, 0x20E);
 }
 
 #[test]
 fn call_return() {
-    let mut c8 = Chip8::new(false);
+    let mut c8 = Chip8::new(true);
 
     // Make sure the stack is initially empty.
     assert_eq!(c8.sp, 0);
@@ -57,21 +48,16 @@ fn call_return() {
     }
 
     // Call a program at 789 and check the stack.
-    c8.opcode = Opcode::new(0x2789);
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x2789);
     assert_eq!(c8.sp, 1);
     assert_eq!(c8.stack[0], 0x200);
-
     for val in c8.stack[1..].iter() {
         assert_eq!(*val, 0, "Found non-zero value in upper part of stack");
     }
 
     // Return from that program and make sure
     // variables are reset.
-    c8.opcode = Opcode::new(0x00EE);
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x00EE);
     assert_eq!(c8.sp, 0);
     for val in c8.stack[1..].iter() {
         assert_eq!(*val, 0, "Found non-zero value in stack");
@@ -80,26 +66,18 @@ fn call_return() {
 
 #[test]
 fn add() {
-    let mut c8 = Chip8::new(false);
+    let mut c8 = Chip8::new(true);
 
-    c8.opcode = Opcode::new(0x7212);
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x7212);
     // Test that value is now correct.
     assert_eq!(c8.registers[2], 0x12);
 
     // Test the max value in a different register.
-    c8.opcode = Opcode::new(0x73FF);
-    c8.decode_execute();
-
-    // Test that value is now correct.
+    run_opcode(&mut c8, 0x73FF);
     assert_eq!(c8.registers[3], 0xFF);
 
     // Now, add one, and test overflow.
-    c8.opcode = Opcode::new(0x7301);
-    c8.decode_execute();
-
-    // Test that value is now correct.
+    run_opcode(&mut c8, 0x7301);
     assert_eq!(c8.registers[3], 0x00);
 }
 
@@ -108,22 +86,17 @@ fn add_with_carry() {
     let mut c8 = Chip8::new(true);
 
     // Test adding the max value without overflow.
-    c8.opcode = Opcode::new(0x73FF); // Add FF to reg 3 (0).
-    c8.decode_execute();
-    c8.opcode = Opcode::new(0x8374); // Add reg 7 (0) to reg 3.
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x73FF); // Add FF to reg 3 (0).
+    run_opcode(&mut c8, 0x8374); // Add reg 7 (0) to reg 3.
 
-    // Test that value is now correct.
+    // Test that values are now correct.
     assert_eq!(c8.registers[3], 0xFF);
     assert_eq!(c8.registers[0xF], 0);
 
     // Now, add one, and test overflow.
-    c8.opcode = Opcode::new(0x7401); // Add 1 to reg 4 (0).
-    c8.decode_execute();
-    c8.opcode = Opcode::new(0x8344); // Add reg 4 (1) to reg 3 (FF).
-    c8.decode_execute();
+    run_opcode(&mut c8, 0x7401); // Add 1 to reg 4 (0).
+    run_opcode(&mut c8, 0x8344); // Add reg 4 (1) to reg 3 (FF).
 
-    // Test that value is now correct.
     assert_eq!(c8.registers[3], 0);
     // Test that the overflow register is correctly set.
     assert_eq!(c8.registers[0xF], 1);
@@ -133,33 +106,24 @@ fn add_with_carry() {
 fn sub() {
     let mut c8 = Chip8::new(true);
 
-    c8.opcode = Opcode::new(0x71A2); // Add A2 to reg 1 (0).
-    c8.decode_execute();
-    c8.opcode = Opcode::new(0x7203); // Add 03 to reg 2 (0).
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x71A2); // Add A2 to reg 1 (0).
+    run_opcode(&mut c8, 0x7203); // Add 03 to reg 2 (0).
     assert_eq!(c8.registers[1], 0xA2);
     assert_eq!(c8.registers[2], 0x03);
 
     // Subtract nothing and check that values are not updated.
-    c8.opcode = Opcode::new(0x8135);
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x8135);
     assert_eq!(c8.registers[1], 0xA2);
     assert_eq!(c8.registers[2], 0x03);
 
     // Subtract 2 (03) from 1 (A2), make sure underflow was not reported.
-    c8.opcode = Opcode::new(0x8125);
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x8125);
     assert_eq!(c8.registers[1], 0x9F);
     assert_eq!(c8.registers[2], 0x03);
     assert_eq!(c8.registers[0xF], 1, "Register underflow was falsely reported!");
 
     // Finally, subtract 1 (9F) from 2 (3), check for underflow.
-    c8.opcode = Opcode::new(0x8215);
-    c8.decode_execute();
-
+    run_opcode(&mut c8, 0x8215);
     assert_eq!(c8.registers[1], 0x9F);
     assert_eq!(c8.registers[2], 0x64);
     assert_eq!(c8.registers[0xF], 0, "Register underflow was falsely reported!");
